e80c4611208e854c0ee0c45b1327d1f56e55248b
Leonardo Marques <leonardo.mateus01@hotmail.com>
2021-10-22 14:20:00 -0300

    Alterando o @Controller para @RestController

--- forum/src/main/java/com/leolmcoding/forum/controller/TopicosController.java
+++ forum/src/main/java/com/leolmcoding/forum/controller/TopicosController.java
@@ -1,14 +1,14 @@
 package com.leolmcoding.forum.controller;
 
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.ArrayList;
 import com.leolmcoding.forum.modelo.*;
-@Controller
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TopicosController {
     @RequestMapping("/topicos")
-    @ResponseBody
     public ArrayList<Topico> lista(){
         Topico topico = new Topico("Duvida","Duvida com Spring",new Curso("Spring","Programacao"));
         ArrayList<Topico> topicos = new ArrayList<Topico>();
